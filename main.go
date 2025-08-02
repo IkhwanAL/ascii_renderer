ff3429b2fc74e6e918a2f70258adc3f02cc40124
IkhwanAl <ikhwanal235@gmail.com>
2025-08-02 12:04:08 +0700

    feat: Create Nearest Neighbor Algorithm to down scale the image

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"log"
+	"math"
 	"os"
 
 	"image/color"
@@ -13,7 +14,7 @@ import (
 
 func main() {
 	// TODO Get Image
-	imgPath := "./img/dice_5.png"
+	imgPath := "./img/secondTest.jpg"
 	
 	reader, err := os.Open(imgPath)
 
@@ -41,7 +42,9 @@ func main() {
 		}
 	}
 
-	outFile, err := os.Create("./img/testResult.jpg")
+	test := DownScaleImage(newImageSet, newImageSet.Bounds().Max.X / 3, newImageSet.Bounds().Max.Y / 3) // 
+	
+	outFile, err := os.Create("./img/testResult1.jpg")
 
 	if err != nil {
 		log.Fatal(err)
@@ -49,11 +52,40 @@ func main() {
 
 	defer outFile.Close()
 
-	err = jpeg.Encode(outFile, newImageSet, nil)
+	err = jpeg.Encode(outFile, test, nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	fmt.Printf("Width  : %d, To %d \n", bounds.Max.X, test.Bounds().Max.X)
+	fmt.Printf("Height : %d, T0 %d \n", bounds.Max.Y, test.Bounds().Max.Y)
 	fmt.Print("Done")
+}
+
+func DownScaleImage(img *image.RGBA, targetWidthScale int, targetHeightScale int) *image.RGBA {
+	imageScale := image.NewRGBA(image.Rect(0,0, targetWidthScale, targetHeightScale)) // Create New Predefined Empty Image
+
+	Sx := float64(img.Bounds().Max.X / targetWidthScale) // Scale X Value
+	Sy := float64(img.Bounds().Max.Y / targetHeightScale) // Scale Y Value
+
+	for y := 0; y < targetHeightScale; y++ {
+		for x := 0; x < targetWidthScale; x++ {
+			originalX := float64(Sx * float64(x)) // Which X Position are in Original X
+			originalY := float64(Sy * float64(y)) // Which Y Position Are in Original Y
+
+			roundedX := int(math.RoundToEven(originalX)) // Round the value (Nearest Neighbor)
+			roundedY := int(math.RoundToEven(originalY)) // Round the value (Nearest Neighbot)
+
+			// This is making sure that rounded pixel value is not overlapped with max original value
+			clappedX := min(max(0, roundedX), img.Bounds().Max.X-1) 
+			clappedY := min(max(0, roundedY), img.Bounds().Max.Y-1)
+
+			pixelColor := img.At(clappedX, clappedY) // Get The Value From Original Image
+
+			imageScale.Set(x, y, pixelColor)
+		}
+	}
+
+	return imageScale
 }
